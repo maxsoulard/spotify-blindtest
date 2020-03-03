a1b9b8b0e7e11ffad3a2da7d901282f16f5d067c
maxsoulard <max.soulard@gmail.com>
2020-03-03 17:51:16 +0100

    fix changedetection issue

--- src/app/spotify-player/player/player.component.ts
+++ src/app/spotify-player/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { SpotifyPlayerService } from '../spotify-player.service';
 import { Store, select } from '@ngrx/store';
 import { map, filter, switchMap, tap } from 'rxjs/operators';
@@ -15,22 +15,28 @@ import { PlayerState } from '../state/play.reducer';
 export class PlayerComponent implements OnInit {
 
   private spotifyInstance: any;
+  displayPlayer: boolean = false;
   trackPlaying: any;
 
-  constructor(private store: Store<any>, private spotifyPlayerService: SpotifyPlayerService, private spotifyBrowseService: SpotifyBrowseService) {
-    // Init spotify SDK
-    (<any>window).onSpotifyWebPlaybackSDKReady = () => {
-      try {
-        this.spotifyPlayerService.getSpotifyInstance(({player, deviceId}) => {
-          this.spotifyInstance = {player, deviceId};
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  constructor(private store: Store<any>,
+    private spotifyPlayerService: SpotifyPlayerService,
+    private spotifyBrowseService: SpotifyBrowseService,
+    private changeDetector: ChangeDetectorRef) {
+      // Init spotify SDK
+      (<any>window).onSpotifyWebPlaybackSDKReady = () => {
+        try {
+          this.spotifyPlayerService.getSpotifyInstance(({player, deviceId}) => {
+            this.spotifyInstance = {player, deviceId};
+            this.displayPlayer = true;
+            this.changeDetector.detectChanges();
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      };
   }
 
-  async ngOnInit() {
+  ngOnInit() {
     this.store.pipe(
       select('player'),
       filter((player: PlayerState) => {
