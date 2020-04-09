3796f87a03ccb21f5fc1b0adca5a1df73808a253
maxsoulard <max.soulard@gmail.com>
2020-04-10 00:36:20 +0200

    feat mutation get or create user

--- backend/src/resolvers/mutation.js
+++ backend/src/resolvers/mutation.js
@@ -1,11 +1,13 @@
+const { userByName } = require('./query')
+
 module.exports = {
   createUser: (root, args, context) => {
-    return context.prisma.createUser({name: args.name, score: 0, gamesCount: 0})
+    return userResult = context.prisma.createUser({name: args.name, score: 0, gamesCount: 0}).catch(() => userByName(root, args, context))
   },
   updateUserScore: (root, args, context) => {
     return context.prisma.updateUser({data: {score: args.score}, where: {id: args.userId}})
   },
   addGameToHistory: (root, args, context) => {
-    return context.prisma.createGameHistory({score: args.score, date: new Date(), user: {connect: {id: args.userId}}});
+    return context.prisma.createGameHistory({score: args.score, date: new Date(), user: {connect: {id: args.userId}}})
   },
 }
