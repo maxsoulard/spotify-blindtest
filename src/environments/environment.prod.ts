94a53cc2db5782b780f242b21726f5709c74e471
maxsoulard <max.soulard@gmail.com>
2020-04-10 11:55:46 +0200

    added environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  spotifyBaseUrl: 'https://accounts.spotify.com',
+  spotifyRedirectUri: `${document.location.origin}/spotify-access`,
+  clientId: 'e22d6ae4c0f943cc8394715461713cff',
+  graphQlUri: 'https://spotify-blindtest-api.maximesoulard.now.sh'
 };
