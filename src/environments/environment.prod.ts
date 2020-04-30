688700c2d0ab728609cd60c7c69b6a66dc3e0aaf
maxsoulard <max.soulard@gmail.com>
2020-04-30 23:51:35 +0200

    env prod true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   spotifyBaseUrl: 'https://accounts.spotify.com',
   spotifyRedirectUri: `${document.location.origin}/spotify-access`,
   clientId: 'e22d6ae4c0f943cc8394715461713cff',
